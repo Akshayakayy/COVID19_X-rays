810633e098e91d7cbb5c77f9e1b2bcf36e9228b2
akshayakayy <akshaya.karthikeyan@research.iiit.ac.in>
2020-04-17 22:32:52 +0530

    accuracy

--- dataloader.py
+++ dataloader.py
@@ -14,10 +14,11 @@ import warnings
 warnings.filterwarnings("ignore")
 
 class CovidLandmarksDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
+    def __init__(self, csv_file, root_dir, model_type="detection", transform=None):
         self.landmarks_frame = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
+        self.model_type = model_type
 
     def __len__(self):
         return len(self.landmarks_frame)
@@ -33,14 +34,24 @@ class CovidLandmarksDataset(Dataset):
         labels = self.landmarks_frame.iloc[idx, 1]
         labels = labels.split('|')
 
-        gt_label = np.zeros((15),np.float32)
+        
         classes = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion','Emphysema',
                    'Fibrosis','Hernia', 'Infiltration', 'Mass', 'Nodule', 'Pleural_Thickening', 'Pneumonia',
                    'Pneumothorax', 'COVID-19']
-        for i in range(15):
+
+        if self.model_type == "classification":
+            gt_label = np.zeros((15),np.float32)
+            for i in range(15):
+                for j in labels:
+                    if j == classes[i]:
+                        gt_label[i] = 1.0
+
+        elif self.model_type == "detection":
+            gt_label = 0.0
             for j in labels:
-                if j == classes[i]:
-                    gt_label[i] = 1.0
+                if j == classes[14]:
+                    gt_label = 1.0
+
         
         # Sample
         sample = {'image': image, 'gt_label': gt_label}
